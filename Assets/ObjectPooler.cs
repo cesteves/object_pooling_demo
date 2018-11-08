dabaeeb24e623c333d7be17019b750f93bbe952d
Christian Esteves via Surface Pro 4 <c.esteves92@gmail.com>
2018-11-08 14:22:33 -0500

    Enable resizable pool

--- Assets/ObjectPooler.cs
+++ Assets/ObjectPooler.cs
@@ -2,40 +2,57 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjectPooler : MonoBehaviour {
+public class ObjectPooler : MonoBehaviour
+{
 
-	List<GameObject> pooledObjects;
+    List<GameObject> pooledObjects;
     GameObject objectToPool;
+    bool resizable;
 
-	public ObjectPooler(GameObject objectToPool, int poolSize)
-	{
-		// Instantiate our pool
-		pooledObjects = new List<GameObject> ();
+    public ObjectPooler(GameObject objectToPool, int poolSize, bool resizable)
+    {
+        // Instantiate our pool
+        pooledObjects = new List<GameObject>();
         this.objectToPool = objectToPool;
+        this.resizable = resizable;
 
-		// Instantiate the amount of objects we need
-		for (int i = 0; i < poolSize; i++) {
-			GameObject obj = Instantiate (objectToPool);
-			// Set each object inactive
-			obj.SetActive (false);
-			// Add it to the pool
-			pooledObjects.Add (obj);
-		}
-	}
+        // Instantiate the amount of objects we need
+        for (int i = 0; i < poolSize; i++)
+        {
+            GameObject obj = Instantiate(objectToPool);
+            // Set each object inactive
+            obj.SetActive(false);
+            // Add it to the pool
+            pooledObjects.Add(obj);
+        }
+    }
 
-	public GameObject getPooledObject(){
-		// Loop through our pool
-		for (int i = 0; i < pooledObjects.Count; i++) {
-			// If we find an object that is not active, return it
-			if (!pooledObjects [i].activeInHierarchy) {
-				return pooledObjects [i];
-			}
-		}
+    public GameObject getPooledObject()
+    {
+        // Loop through our pool
+        for (int i = 0; i < pooledObjects.Count; i++)
+        {
+            // If we find an object that is not active, return it
+            if (!pooledObjects[i].activeInHierarchy)
+            {
+                return pooledObjects[i];
+            }
+        }
 
-        GameObject newObject = Instantiate (objectToPool);
-        pooledObjects.Add(newObject);
+        // If we can resize this pool, do so.
+        if (resizable)
+        {
+            // If the pool is empty, instantiate a new object and add it to the pool
+            GameObject newObject = Instantiate(objectToPool);
+            pooledObjects.Add(newObject);
 
-		// There are no inactive objects - the pool is empty
-		return newObject;
-	}
+            // Return the newly instantated 
+            return newObject;
+        }
+        else
+        {
+            // Return null if we cannot resize this pool
+            return null;
+        }
+    }
 }
